f1f37327e60e0c96261a3d0d31bbaa5a56133b2d
tanaypatankar <tanaypatankar.2000@gmail.com>
2023-09-23 13:16:48 -0500

    new movies and procfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,11 @@ more_movies = [
     {:title => 'Nomadland', :rating => 'R',
       :release_date => '19-Feb-2021'},
     {:title => 'CODA', :rating => 'PG-13',
-      :release_date => '13-Aug-2021'}
+      :release_date => '13-Aug-2021'},
+    {:title => 'How To Train Your Dragon', :rating => 'PG',
+    :release_date => '21-March-2010'},
+    {:title => 'Kingsman: Secret Service', :rating => 'R',
+        :release_date => '12-Feb-2015'}
   ]
   
   more_movies.each do |movie|
